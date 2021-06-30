32aa3d51f3495723851ae631f9c7375b4fea6120
Szymon Zawrotny <szymonzawrotny@gmail.com>
2021-06-30 17:00:52 +0200

    scroll Box is ready

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 //przewijanie strony na start 
 const toUp = document.querySelector('.toUp');
+const header = document.querySelector('header');
+const foto = document.querySelector('.foto');
+
 toUp.addEventListener('click',()=> {
     $('body, html').animate({
         scrollTop: 0
-    })
-})
+    });
+});
+
+window.addEventListener('scroll',()=> {
+
+    if(scrollY > foto.clientHeight)
+    {
+        toUp.classList.add('active');
+    }
+    else
+    {
+        toUp.classList.remove('active');
+    }
+});
