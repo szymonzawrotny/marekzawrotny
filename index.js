54fd4f0723036ed66cdae6372fbf37ca1c9fdfc3
Szymon Zawrotny <szymonzawrotny@gmail.com>
2021-07-02 16:35:12 +0200

    do header and center the foto

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const toUp = document.querySelector('.toUp');
 const header = document.querySelector('header');
 const foto = document.querySelector('.foto');
+const ul = document.querySelector('header .option ul');
 
 toUp.addEventListener('click',()=> {
     $('body, html').animate({
@@ -11,12 +12,16 @@ toUp.addEventListener('click',()=> {
 
 window.addEventListener('scroll',()=> {
 
-    if(scrollY > foto.clientHeight)
+    if(scrollY > foto.clientHeight/2)
     {
         toUp.classList.add('active');
+        header.classList.add('active'); //zmiana koloru headera
+        ul.classList.add('active');
     }
     else
     {
         toUp.classList.remove('active');
+        header.classList.remove('active');
+        ul.classList.remove('active');
     }
 });
