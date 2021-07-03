fd5dd97ad10a7d3d7339d956f0052f987f223f05
Szymon Zawrotny <szymonzawrotny@gmail.com>
2021-07-03 21:52:45 +0200

    add hamburger menu, toUp to mobile version and change color of template

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const toUp = document.querySelector('.toUp');
 const header = document.querySelector('header');
 const foto = document.querySelector('.foto');
+const aboutMe = document.querySelector('.aboutMe');
+const galery = document.querySelector('.galery');
 const ul = document.querySelector('header .option ul');
 
 toUp.addEventListener('click',()=> {
@@ -24,4 +26,39 @@ window.addEventListener('scroll',()=> {
         header.classList.remove('active');
         ul.classList.remove('active');
     }
+});
+
+// nawigacja i odsyłacze 
+
+const li1 = document.querySelector('li:nth-of-type(1)');
+const li2 = document.querySelector('li:nth-of-type(2)');
+const li3 = document.querySelector('li:nth-of-type(3)');
+
+li1.addEventListener('click',()=> {
+    $('body, html').animate({
+        scrollTop: foto.clientHeight- header.clientHeight
+    });
+});
+
+li2.addEventListener('click',()=> {
+    $('body, html').animate({
+        scrollTop: foto.clientHeight + aboutMe.clientHeight - header.clientHeight
+    });
+});
+
+li3.addEventListener('click',()=> {
+    $('body, html').animate({
+        scrollTop: foto.clientHeight + aboutMe.clientHeight + galery.clientHeight
+    });
+});
+
+//hamburger menu 
+
+const hamburger = document.querySelector('.hamburger');
+const aside = document.querySelector('aside');
+
+hamburger.addEventListener('click',()=> {
+
+    hamburger.classList.toggle('active');
+    aside.classList.toggle('active');
 });
